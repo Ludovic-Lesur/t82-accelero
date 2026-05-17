a5dc0ec50eee014f01d0d77abd06de46ffa3f1d9
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-05-17 09:40:56 +0200

    Add GitHub actions workflow.

---
+++ script/git_version.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+sscanf() {
+    local str="$1"
+    local format="$2"
+    [[ "$str" =~ $format ]]
+}
+
+# Files location.
+main_file="../application/src/main.c"
+version_file="../application/inc/version.h"
+
+# Touch files to force makefile operation.
+touch $main_file
+touch $version_file
+
+# Get current date.
+date=`date -R`
+
+# Execute git commands.
+git_version=`git describe --long --always`
+diff=`git diff ':(exclude)../application/inc/sen15901_emulator_flags.h' ':(exclude)../script'`
+
+# Extract fields
+sscanf $git_version "sw([0-9]+).([0-9]+)-([0-9]+)-g(.+)"
+
+# Manage dirty flag.
+dirty_flag=0
+if [[ $diff ]]; then
+    dirty_flag=1
+fi
+
+echo "/*" > $version_file
+echo " * version.h" >> $version_file
+echo " *" >> $version_file
+echo " * Auto-generated on: $date" >> $version_file
+echo " * Author: Ludo" >> $version_file
+echo " */" >> $version_file
+echo "" >> $version_file
+echo "#ifndef __VERSION_H__" >> $version_file
+echo "#define __VERSION_H__" >> $version_file
+echo "" >> $version_file
+echo "#define GIT_VERSION       \"$git_version\"" >> $version_file
+echo "#define GIT_MAJOR_VERSION ${BASH_REMATCH[1]}" >> $version_file
+echo "#define GIT_MINOR_VERSION ${BASH_REMATCH[2]}" >> $version_file
+echo "#define GIT_COMMIT_INDEX  ${BASH_REMATCH[3]}" >> $version_file
+echo "#define GIT_COMMIT_ID     0x${BASH_REMATCH[4]}" >> $version_file
+echo "#define GIT_DIRTY_FLAG    $dirty_flag" >> $version_file
+echo "" >> $version_file
+echo "#endif /* __VERSION_H__ */" >> $version_file
